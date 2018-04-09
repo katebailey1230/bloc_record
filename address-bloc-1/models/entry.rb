0110ef409905344c535530e0efdbe388e9739381
K.Bailey <katebailey1230@gmail.com>
2018-04-09 15:12:46 -0400

    Complete

--- address-bloc-1/models/entry.rb
+++ address-bloc-1/models/entry.rb
@@ -3,6 +3,6 @@ require 'bloc_record/base'
 class Entry < BlocRecord::Base
 	 
   def to_s
-    "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
+    "Name: #{name}\nPhone Number: #{phone}\nEmail: #{email}"
   end
 end
