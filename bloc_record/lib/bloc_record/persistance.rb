280d0ea5d942842b126b3f2e988f8d4593092919
K.Bailey <katebailey1230@gmail.com>
2018-04-11 08:09:10 -0400

    checkpoint_6_done

--- bloc_record/lib/bloc_record/persistance.rb
+++ bloc_record/lib/bloc_record/persistance.rb
@@ -36,11 +36,30 @@ def update_attributes(updates)
      self.class.update(self.id, updates)
    end
 
+def destroy
+     self.class.destroy(self.id)
+   end
+
    module ClassMethods
 
 	def update_all(updates)
        update(nil, updates)
      end
+
+def destroy(*id)
+       if id.length > 1
+         where_clause = "WHERE id IN (#{id.join(",")});"
+       else
+         where_clause = "WHERE id = #{id.first};"
+       end
+
+       connection.execute <<-SQL
+         DELETE FROM #{table} #{where_clause}
+       SQL
+ 
+       true
+     end
+	end
 
      def create(attrs)
        attrs = BlocRecord::Utility.convert_keys(attrs)
@@ -75,6 +94,25 @@ def update(ids, updates)
          UPDATE #{table}
          SET #{updates_array * ","} #{where_clause}
        SQL
+
+       true
+     end
+
+def destroy_all(conditions_hash=nil)
+       if conditions_hash && !conditions_hash.empty?
+         conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
+         conditions = conditions_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+ 
+         connection.execute <<-SQL
+           DELETE FROM #{table}
+           WHERE #{conditions};
+         SQL
+       else
+
+       connection.execute <<-SQL
+         DELETE FROM #{table}
+       SQL
+ 	  end
 
        true
      end
