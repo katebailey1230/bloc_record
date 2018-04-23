8840c02d8d509794c6c8fa064e3fc106d94ccbe1
K.Bailey <katebailey1230@gmail.com>
2018-04-23 11:26:28 -0400

    revisions.2

--- bloc_record/lib/bloc_record.rb
+++ bloc_record/lib/bloc_record.rb
@@ -1,6 +1,12 @@
 module BlocRecord
-   def self.connect_to(filename)
+   def self.connect_to(filename, :sqlite3)
      @database_filename = filename
+	 @connect_to("db/address_bloc.db", :sqlite3)
+   end
+	
+	def self.connect_to(filename, :pg)
+     @database_filename = filename
+	 @connect_to("db/address_bloc.db", :pg)
    end
  
    def self.database_filename
--- bloc_record/lib/bloc_record/connection.rb
+++ bloc_record/lib/bloc_record/connection.rb
@@ -1,8 +1,12 @@
 require 'pg'
 require 'sqlite3'
+
  
  module Connection
    def connection
-     @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+     IF @connection ||= SQLite3::Database.new(BlocRecord.database_filename)?
+	   BlocRecord.connect_to("db/address_bloc.db", :sqlite3)
+	   else
+	   BlocRecord.connect_to("db/address_bloc.db", :pg)   
    end
- end
+end
